8a518ad0e7ce98d792a61a827c03d5505ccaaa71
CodingC1402 <CornyCodingCorn@gmail.com>
2023-01-28 19:09:18 +0700

    clean up after merge

--- src/plugins/tilemap.rs
+++ src/plugins/tilemap.rs
@@ -1,6 +1,6 @@
 use std::collections::{HashMap, HashSet};
 
-use crate::{entities::{Tile, TileBundle}, components::tilemap::Platform};
+use crate::{entities::{TerrainBundle, TrapBundle, WebStickerBundle, Terrain, WebSticker, Trap}, components::tilemap::Platform};
 use bevy::prelude::*;
 use bevy_ecs_ldtk::{prelude::*, GridCoords};
 use bevy_rapier2d::prelude::*;
